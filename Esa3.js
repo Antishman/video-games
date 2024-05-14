c05cbdfbe41d2efa02cfaf5a386640c14001e302
esayasetana <esayasetana3@gmail.com>
2024-05-15 00:02:30 +0300

    EE3
    EE3

--- Esa3.js
+++ Esa3.js
@@ -47,9 +47,70 @@ function shuffleArray(array) {
     }
     return array;
 }
-/*
+
 function intRandom(min, max) {
     var rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
-*/
+// GLOBAL VARIABLES
+var moves = 0,
+    winner = 0,
+    x = 1,
+    o = 3,
+    player = x,
+    computer = o,
+    whoseTurn = x,
+    gameOver = false,
+    score = {
+        ties: 0,
+        player: 0,
+        computer: 0
+    },
+    xText = "Ã—",
+    oText = "o",
+    playerText = xText,
+    computerText = oText,
+    difficulty = 1,
+    myGrid = null;
+
+//==================================
+// GRID OBJECT
+//==================================
+
+// Grid constructor
+//=================
+function Grid() {
+    this.cells = new Array(9);
+}
+
+// Grid methods
+//=============
+
+// Get free cells in an array.
+// Returns an array of indices in the original Grid.cells array, not the values
+// of the array elements.
+// Their values can be accessed as Grid.cells[index].
+Grid.prototype.getFreeCellIndices = function () {
+    var i = 0,
+        resultArray = [];
+    for (i = 0; i < this.cells.length; i++) {
+        if (this.cells[i] === 0) {
+            resultArray.push(i);
+        }
+    }
+    // console.log("resultArray: " + resultArray.toString());
+    // debugger;
+    return resultArray;
+};
+
+// Get a row (accepts 0, 1, or 2 as argument).
+// Returns the values of the elements.
+Grid.prototype.getRowValues = function (index) {
+    if (index !== 0 && index !== 1 && index !== 2) {
+        console.error("Wrong arg for getRowValues!");
+        return undefined;
+    }
+    var i = index * 3;
+    return this.cells.slice(i, i + 3);
+};
+
