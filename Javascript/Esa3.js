b8b38bbdc7e120acadfa02bc63750117449e16fe
Hassenyasin <165617473+Hassenyasin@users.noreply.github.com>
2024-05-16 00:23:08 +0600

    Update Esa3.js

--- Javascript/Esa3.js
+++ Javascript/Esa3.js
@@ -219,6 +219,9 @@ Grid.prototype.getFirstWithTwoInARow = function (agent) {
     return false;
 };
 
+
+
+
 Grid.prototype.reset = function () {
     for (var i = 0; i < this.cells.length; i++) {
         this.cells[i] = 0;
