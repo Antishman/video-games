b9cb924dea26fb430ed5322726dd124054a92181
kaleab tamrat <164320962+kaleabtamrat@users.noreply.github.com>
2024-05-16 19:37:59 +0300

    hangmanjava2

--- Hangman.java
+++ Hangman.java
@@ -167,4 +167,33 @@ window.onload = function() {
       el.classList.remove(myclass);
     }
   }
+function startNewGame() {
+    let btnWrapper = document.querySelector(".button-wrapper");
+    let winMessage = document.getElementById("statusMessage");
+    let wrongLetters = document.querySelector("#wrongLetters span");
+    let warningText = document.querySelector("#warningText");
+    let hiddenHangman = Array.from(document.querySelectorAll("svg .bodyPart"));
+    let hiddenLives = Array.from(document.querySelectorAll(".lives"));
+
+    for (let bodyPart of hiddenHangman) {
+      hideElements("hidden", bodyPart);
+    }
+
+    for (let life of hiddenLives) {
+      unhideElements("hiddenLife", life);
+    }
+
+    wrongLetters.innerHTML = "";
+    unhideElements("hidden", btnWrapper);
+    hideElements("hidden", wrongLetters.parentNode, warningText);
+    winMessage.innerHTML = "Vanilla JavaScript Hangman Game";
+    winMessage.style.color = "black";
+    let oldP = document.getElementById("placeholderP");
+    if (oldP.parentNode) {
+      oldP.parentNode.removeChild(oldP);
+    }
+
+    let startGame = new Hangman();
+    startGame.setupNewWord();
+  }
 
